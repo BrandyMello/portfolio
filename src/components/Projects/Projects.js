23b370404307fb72abcabe74cafe7d02588fcfda
Brandy Mello <brandyamello@gmail.com>
2020-02-09 15:16:36 -0700

    Import and add arrows to scroll images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Projects.css';
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 const Project = () => (
   <main>
-    <div class="border">
-      <div class="projectDisplay">
+    <div className="border">
+      <div className="projectDisplay">
+        <IoIosArrowBack className="icon_back_arrow"/>
+        <IoIosArrowForward className="icon_forward_arrow"/>
       </div>
     </div>
   </main>
